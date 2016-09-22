bac2e4b4a739748fff767119fef1e40ea79e83eb
Cloudaerius <Cloudaerius@172.16.15.137>
2016-09-22 05:49:32 -0500

    Lab 2 - Student Complete

---
+++ src/dip/lab2/student/solution1/Tip.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author amellem
+ */
+public interface Tip {
+
+    public abstract ServiceQuality getServiceQuality();
+
+    public abstract double getTip();
+
+    public abstract void setServiceRating(ServiceQuality q);
+    
+}
---
+++ src/dip/lab2/student/solution1/TipManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author amellem
+ */
+public class TipManager {
+    
+    public void doTip(Tip tip){
+        
+        
+        System.out.println(tip.getTip());
+    }
+}
